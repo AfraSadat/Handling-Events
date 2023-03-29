fcb524be5dfffd81d5c76aae574d1cbf9368e7b8
Afra Sadat <afrasadaa1@gmail.com>
2023-03-30 01:04:14 +0500

    Handling Events

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App=() =>{
+  const purple ="8e44ad";
+  const [bg, setBg]= useState(purple);
+  const [name, setName] = useState("Click me");
+const bgChange=()=>{
+  //console.log('clicked');
+  let newBg='#34495e';
+  setBg(newBg);
+  setName('Oh Nooo!!!: ');
+  
+};
+  return(
+    <>
+    <div style={ { backgroundColor: bg}} >
+      <button onClick={bgChange}>{name}</button>
+     
     </div>
+    </>
   );
-}
-
+};
 export default App;
